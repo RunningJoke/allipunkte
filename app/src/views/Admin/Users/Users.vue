ad73a38b2793d1a56fe21688e893d935300ce267
Patrick Schwederski <schwederski@alli-go.de>
2022-08-05 00:17:11 +0200

    enhanced the user list with search, filter and pagination

--- app/src/views/Admin/Users/Users.vue
+++ app/src/views/Admin/Users/Users.vue
@@ -1,26 +1,65 @@
 <template>
-    <div class="text-left"><h4>Benutzerlisten</h4>
-
-        <b-button variant="success" v-b-modal.createNewUser>
-            <b-icon-person />
+    <div class="text-left m-1"><h4>Benutzerlisten</h4>
+    <div class="d-flex flex-row mb-2 align-items-center">
+        
+        <b-button variant="success" v-b-modal.createNewUser class="mr-2 text-nowrap" size="sm">
+            + <b-icon-person />
         </b-button>
+
+        <b-pagination
+            size="sm"
+            class="mb-0 mx-2"
+            hide-goto-end-buttons
+            v-model="currentPage"
+            :total-rows="userItems.length"
+            :per-page="perPage"
+        ></b-pagination>
+
+        <b-input-group class="flex-nowrap justify-content-end mr-2">
+            <b-input-group-prepend is-text>
+                <b-icon-files />
+            </b-input-group-prepend>
+            <b-form-input style="max-width: 80px;" type="number" :step="10" v-model="perPage"></b-form-input>           
+        </b-input-group>
+
+
+         <b-input-group class="flex-nowrap" style="width: 600px;">
+            <b-input-group-prepend is-text>
+                <b-icon-search />
+            </b-input-group-prepend>
+            <b-form-input placeholder="Suche..." type="search" v-model="filterItem.filterString"></b-form-input>
+            <b-input-group-append is-text>
+                <b-form-checkbox switch :value="true" :unchecked-value="false" v-model="filterItem.hideWithTargetPoints" >
+                    ohne Zielwert
+                </b-form-checkbox>
+            </b-input-group-append>            
+        </b-input-group>
+
+     
+
+    </div>
 
         <b-table
             striped 
             hover 
+            :per-page="perPage"
+            :current-page="currentPage"
+            :filter="filterItem"
+            :filter-function="filterRowsFunction"
             :items="userItems" 
             :fields="fields">
             <template #cell(actions)="row">
                 <div class="d-flex flex-nowrap">
-                    <b-button size="sm" @click="detailUser = row.item" v-b-modal.displayUser class="mr-1">
-                    Details
-                    </b-button>
                     <b-input-group prepend="Neuer Zielwert" size="sm">
                         <b-form-input type="number" v-model="newTarget" style="max-width: 100px;"></b-form-input>
                             <b-input-group-append>
                                 <b-button variant="outline-success" @click="updateTargetScore(row.item)"><b-icon-arrow-clockwise /></b-button>
                             </b-input-group-append>
                     </b-input-group>
+                    
+                    <b-button size="sm" @click="detailUser = row.item" v-b-modal.displayUser class="mr-1">
+                    Details
+                    </b-button>
                 </div>
             </template>
         </b-table>
@@ -60,7 +99,7 @@ export default {
             return vm.$store.getters.getUsers.map(user => {
                 let currentScore = user?.scores?.find(score => score?.cycle?.id == this.$store.getters.getCurrentCycle?.id) ?? null
                 let targetAmount = currentScore.targetAmount ?? -1
-                let enhancedUser = {...user}
+                let enhancedUser = {...user, 'targetAmount' : targetAmount}
                 if(targetAmount > 0) {
                  Object.assign(enhancedUser, {'_rowVariant' : 'success'})
                 }
@@ -69,6 +108,16 @@ export default {
         }
     },
     methods: {
+        filterRowsFunction: function(user,filterObj) {
+            //filter should check lastname, firstname, mail, username
+            if(filterObj.hideWithTargetPoints) {
+                if(user.targetAmount > 0) {
+                    return false
+                }
+            }
+            let fs = filterObj.filterString
+            return user.lastname.includes(fs) || user.firstname.includes(fs) || user.mail.includes(fs) || user.username.includes(fs)
+        },
         updateTargetScore: async function(targetUser) {
              try {
                 await this.$request.sendJsonRequest(this.$store.getters.baseUrl+'api/users/'+targetUser.id+'/setTargetPoints' , 'POST', {"newTargetScore" : this.newTarget})
@@ -113,6 +162,12 @@ export default {
     },
     data() {
       return {
+        currentPage: 1,
+        perPage: 50,
+        filterItem: {
+            filterString: "",
+            hideWithTargetPoints: false
+        },
         newTarget: 0,
         newUser: {},
         detailUser: {},
