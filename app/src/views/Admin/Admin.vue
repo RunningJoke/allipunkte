ff6db92c5133df58800b81f31c250a0241e6373b
RunningJoke <47787778+RunningJoke@users.noreply.github.com>
2022-08-05 00:38:09 +0200

    25 add filteringsearching to user admin table (#37)
    * enhanced the user list with search, filter and pagination
    
    * made the search bar sticky
    
    * style changes for better sticky support
    
    * reworked admin menu bar
    
    Co-authored-by: Patrick Schwederski <schwederski@alli-go.de>

--- app/src/views/Admin/Admin.vue
+++ app/src/views/Admin/Admin.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <b-nav class="admin-navbar">
-      <b-link to="/admin/users" >Benutzer</b-link>
-      <b-link to="/admin/cycles" >Zyklen</b-link>
+    <b-nav class="admin-navbar" tabs>
+      <b-nav-item  to="/admin/users" active-class="active">Benutzer</b-nav-item>
+      <b-nav-item  to="/admin/cycles" active-class="active" >Zyklen</b-nav-item >
     </b-nav>
     <router-view/>
   </div>
