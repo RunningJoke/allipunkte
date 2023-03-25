719e8ff03e8c4fcd02f2ecc575ab32ea9bfdcf6c
Patrick Schwederski <schwederski@alli-go.de>
2023-03-25 22:19:35 +0100

    added the password reset functionality

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -10,4 +10,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
---
+++ api/src/Form/ChangePasswordFormType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ChangePasswordFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => [
+                    'attr' => [
+                        'autocomplete' => 'new-password',
+                        'class' => 'form-control'
+                    ],
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Bitte gib ein Passwort an',
+                        ]),
+                        new Length([
+                            'min' => 6,
+                            'minMessage' => 'Dein Passwort muss mindestens {{ limit }} Zeichen umfassen',
+                            // max length allowed by Symfony for security reasons
+                            'max' => 4096,
+                        ]),
+                    ],
+                    'label' => 'Neues Passwort',
+                ],
+                'second_options' => [
+                    'attr' => ['autocomplete' => 'new-password',
+                                'class' => 'form-control'],
+                    'label' => 'Passwort wiederholen',
+                ],
+                'invalid_message' => 'Die Passwörter müssen übereinstimmen.',
+                // Instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([]);
+    }
+}
